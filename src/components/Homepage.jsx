18c133eb2bce6aa4719be9ea372ed618f4d559dc
rendinel <rendinel@protonmail.com>
2021-09-23 13:09:19 +0200

    push

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+//package that format number
+import millify from 'millify'
+import { Typography, Row, Col, Statistic } from 'antd'
+import { Link } from 'react-router-dom'
+//we deconstruct so we can write <Title> instead of <Typography.Title> 37
+const { Title } = Typography
+
+const Homepage = () => {
+  return (
+    <>
+      <Title level={2} className='heading'>
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title='Total Cryptocurrencies' value='5'></Statistic>
+        </Col>
+        <Col span={12}>
+          <Statistic title='Total Exchanges' value='5'></Statistic>
+        </Col>
+        <Col span={12}>
+          <Statistic title='Total Market Cap' value='5'></Statistic>
+        </Col>
+        <Col span={12}>
+          <Statistic title='Total 24h Volume' value='5'></Statistic>
+        </Col>
+        <Col span={12}>
+          <Statistic title='Total Markets' value='5'></Statistic>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default Homepage
