477bd535a4ecfddadf73eb479bff4918d05e9e3b
rendinel <rendinel@protonmail.com>
2021-09-23 23:11:08 +0200

    push

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -7,14 +7,19 @@ const cryptoApiHeaders = {
 //endpoint base
 const baseUrl = 'https://coinranking1.p.rapidapi.com'
 
+//function that accept url as a parameter and return url:url and headers:cryptoApiHeaders
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 //fetch the data from the api
 //03 we pass count so we can decide how many currency we want to display, we also pass the count as a limit to our endpoint so we can display currency based on the count
 export const cryptoApi = createApi({
+  //reducer path simply say what this reducer is for and we pass a string as a name
   reducerPath: 'cryptoApi',
+  //we define the basequery as the fetchbasequery function from the reduxtoolkit to whom we pass our baseurl
   baseQuery: fetchBaseQuery({ baseUrl }),
+  //endpoints is a function that have builder as a parameter and return an obj where we name our endpoint and are equal to builder.query to which we pass an obj with a query that is equal to function that point in the direction of that specific request,
   endpoints: (builder) => ({
     getCryptos: builder.query({
+      //we invoke createrequest and pass to it our url as a params so we can write our url for the search
       query: (count) => createRequest(`/coins?limit=${count}`),
     }),
   }),
