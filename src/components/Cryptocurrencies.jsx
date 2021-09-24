3be9705360ae5a57819a4a14aa2cd6c139403b32
rendinel <rendinel@protonmail.com>
2021-09-24 13:46:17 +0200

    push

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import millify from 'millify'
 import { Link } from 'react-router-dom'
 import { Card, Row, Col, Input } from 'antd'
@@ -10,12 +10,32 @@ const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100
   //03 we grab our data from useGetCryptosQuery and rename them cryptosList and also grab isFetching,we also pass count to our hook so we decide how many currency we display, we also pass count inside our cryptoApi
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
-  //03 we set this usestate and pass cryptosList.data.coins to acess the coins, don't know why with ?. instead of just .
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+  //03 we set this usestate and pass cryptosList.data.coins to acess the coins, don't know why with ?. instead of just .,04 we set to be an empty array bevause we pass the data from the useeffect
+  const [cryptos, setCryptos] = useState([])
   console.log(cryptos)
+  //04 usestate to set the searchterm from the input
+  const [searchTerm, setSearchTerm] = useState('')
+  //04 we filter our cryptolist and return the coin that match the typed word,if we don't type nothing we return all hte coin because our initial state it's an empty string
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    setCryptos(filteredData)
+  }, [cryptosList, searchTerm])
+
   if (isFetching) return 'Loading...'
   return (
     <>
+      {/* 04 if simpliefied is true like in the cryptocurrencies component we display the input otherwise we will not display it like in the homepage  */}
+      {!simplified && (
+        <div className='search-crypto'>
+          {/* 04 input that on change set the setserachterm to the word we typed in */}
+          <Input
+            placeholder='Search Cryptocurrency'
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
       <Row className='crypto-card-container' gutter={[32, 32]}>
         {/* we add the ? so that if cryptos is undefined we don't run into error */}
         {cryptos?.map((currency) => {
